7487b8cc469dae76c5763899dbf6cab2b43b03ce
prajoshjohny10329 <prajoshjohny10329@gmail.com>
2024-05-23 19:56:49 +0530

    change static year to award winning

--- assets/js/year.js
+++ assets/js/year.js
@@ -1,5 +1,6 @@
-
+console.log('year js loading');
 const yearI1d = document.getElementById('year-id');
+const trustedH2 = document.getElementById('trusted-h2')
 const currentDate  = new Date().getFullYear()
 const startDate = 1999
 
@@ -7,4 +8,5 @@ const totalYears = Math.abs(currentDate - startDate)
 
 console.log(totalYears);
 
-yearI1d.innerHTML = totalYears + ' Years of Quality Service in Dental <span>Practice</span><img src="assets/img/section-shape-2.webp" alt="Image"></img>'
+yearI1d ? yearI1d.innerHTML = totalYears + ' Years of Quality Service in Dental <span>Practice</span><img src="assets/img/section-shape-2.webp" alt="Image"></img>' : ''
+trustedH2 ? trustedH2.innerHTML = `We Are A ${totalYears}-Year Trusted  & Certified <span>Hospital</span><img src="assets/img/section-shape-2.webp" alt="Image">`: ''
